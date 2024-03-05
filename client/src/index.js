950ac19acc1694cd9d7d01224465cacc09071bc9
luen2003 <100186050+21020776TaDinhLuong@users.noreply.github.com>
2024-03-06 00:50:51 +0700

    final

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import App from './App'
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
@@ -12,4 +13,4 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 )
-
+registerServiceWorker()
