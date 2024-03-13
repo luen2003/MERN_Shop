c7b1b68770ed3175dd1e15889583fd2b00597d0f
luen2003 <100186050+21020776TaDinhLuong@users.noreply.github.com>
2024-03-13 11:45:55 +0700

    final

--- client/src/layout/NavBar.js
+++ client/src/layout/NavBar.js
@@ -56,73 +56,66 @@ export const NavBar = () => {
                 </a>
               </LinkContainer>
             </li>
-            <div
-              className='collapse navbar-collapse'
-              id='navbarSupportedContent'
-            >
-              <ul className='navbar-nav ms-auto'>
-                <li className='nav-item dropdown'>
-                  <a
-                    className='nav-link dropdown-toggle'
-                    href='/'
-                    id='navbarDropdown'
-                    role='button'
-                    data-bs-toggle='dropdown'
-                    aria-haspopup='true'
-                    aria-expanded='false'
-                  >
-                    {userInfo ? userInfo.name : <i className='fa fa-user'></i>}
-                    {/* <i className='fa fa-user'></i> Me */}
-                  </a>
-                  {/* Here's the magic. Add the .animate and .slideIn classes to your .dropdown-menu and you're all set! */}
-                  <div
-                    className='dropdown-menu dropdown-menu-end animate slideIn'
-                    aria-labelledby='navbarDropdown'
-                  >
-                    {userInfo ? (
-                      //   <a className='dropdown-item' href='/'>
-                      //   Another action
-                      // </a>
-                      <div title={userInfo.name} id='username'>
-                        <LinkContainer to='/profile'>
-                          <a className='dropdown-item'>My Profile</a>
-                        </LinkContainer>
-                        <a className='dropdown-item' onClick={logoutHandler}>
-                          Logout
-                        </a>
-                        {/* <NavDropdown.Item onClick={logoutHandler}>
+            <li className='nav-item dropdown'>
+              <a
+                className='nav-link dropdown-toggle'
+                href='/'
+                id='navbarDropdown'
+                role='button'
+                data-bs-toggle='dropdown'
+                aria-haspopup='true'
+                aria-expanded='false'
+              >
+                {userInfo ? userInfo.name : <i className='fa fa-user'></i>}
+                {/* <i className='fa fa-user'></i> Me */}
+              </a>
+              {/* Here's the magic. Add the .animate and .slideIn classes to your .dropdown-menu and you're all set! */}
+              <div
+                className='dropdown-menu dropdown-menu-end animate slideIn'
+                aria-labelledby='navbarDropdown'
+              >
+                {userInfo ? (
+                  //   <a className='dropdown-item' href='/'>
+                  //   Another action
+                  // </a>
+                  <div title={userInfo.name} id='username'>
+                    <LinkContainer to='/profile'>
+                      <a className='dropdown-item'>My Profile</a>
+                    </LinkContainer>
+                    <a className='dropdown-item' onClick={logoutHandler}>
+                      Logout
+                    </a>
+                    {/* <NavDropdown.Item onClick={logoutHandler}>
                           Logout
                         </NavDropdown.Item> */}
-                      </div>
-                    ) : (
-                      <LinkContainer to='/login'>
-                        <Nav.Link>
-                          <i className='fas fa-user'></i> Sign In
-                        </Nav.Link>
-                      </LinkContainer>
-                    )}
+                  </div>
+                ) : (
+                  <LinkContainer to='/login'>
+                    <Nav.Link>
+                      <i className='fas fa-user'></i> Sign In
+                    </Nav.Link>
+                  </LinkContainer>
+                )}
 
-                    {userInfo && userInfo.isAdmin && (
-                      <>
-                        <div className='dropdown-divider' />
-                        <LinkContainer to='/admin/userlist'>
-                          <a className='dropdown-item'>Shoppers</a>
-                        </LinkContainer>
-                        <LinkContainer to='/admin/productlist'>
-                          <a className='dropdown-item'>Products</a>
-                        </LinkContainer>
-                        <LinkContainer to='/admin/orderlist'>
-                          <a className='dropdown-item'>Orders</a>
-                        </LinkContainer>
-                      </>
-                    )}
-                    {/* <a className='dropdown-item' href='/'>
+                {userInfo && userInfo.isAdmin && (
+                  <>
+                    <div className='dropdown-divider' />
+                    <LinkContainer to='/admin/userlist'>
+                      <a className='dropdown-item'>Shoppers</a>
+                    </LinkContainer>
+                    <LinkContainer to='/admin/productlist'>
+                      <a className='dropdown-item'>Products</a>
+                    </LinkContainer>
+                    <LinkContainer to='/admin/orderlist'>
+                      <a className='dropdown-item'>Orders</a>
+                    </LinkContainer>
+                  </>
+                )}
+                {/* <a className='dropdown-item' href='/'>
                       Something else here
                     </a> */}
-                  </div>
-                </li>
-              </ul>
-            </div>
+              </div>
+            </li>
           </ul>
         </div>
       </div>
