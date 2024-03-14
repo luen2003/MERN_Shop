109b28453a3da3655de3c6625902fa8e3f262be1
luen2003 <100186050+21020776TaDinhLuong@users.noreply.github.com>
2024-03-14 17:32:12 +0700

    final

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -21,6 +21,7 @@ const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({ ...keyword })
     .limit(pageSize)
     .skip(pageSize * (page - 1))
+    .sort('-updatedAt')
 
   res.json({ products, page, pages: Math.ceil(count / pageSize) })
 })
